263b3a173486fdf98f0a4f21230f96ca740eb8aa
Andrew Butcher <abutcher@redhat.com>
2015-04-26 17:04:18 -0400

    More cart tests.

--- test/test_cart.py
+++ test/test_cart.py
@@ -26,9 +26,13 @@ class TestCart(TestCase):
     def test_cart_without_provided_description(self):
         """A cart without items is empty"""
         cart = Cart('test-cart')
+        self.assertEqual(cart.is_empty(), True)
         self.assertEqual(cart.repo_items_hash, {})
 
     def test_cart_with_provided_description(self):
         """A cart with items is not empty"""
         cart = Cart('test-cart', [['test-repo', 'share/juicer/empty-0.1-1.noarch.rpm']])
         self.assertEqual(cart.repos(), ['test-repo'])
+        self.assertEqual(cart.keys(), ['test-repo'])
+        self.assertEqual(cart.is_empty(), False)
+        self.assertIsInstance(str(cart), str)

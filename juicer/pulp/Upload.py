60520b509ceddbc92873c1ace67d66033cd1051b
Andrew Butcher <abutcher@redhat.com>
2015-05-03 22:21:45 -0400

    Add an upload progressbar based on the bitmath demo.

--- juicer/pulp/Upload.py
+++ juicer/pulp/Upload.py
@@ -15,91 +15,113 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import bitmath
+import bitmath.integrations
 import hashlib
-from juicer.common import Constants
-from juicer.pulp.Pulp import Pulp
-from juicer.pulp.Repo import Repo
-from pyrpm.rpm import RPM as PYRPM
-from pulp.bindings.upload import UploadAPI
 import os
+import progressbar
+from pyrpm.rpm import RPM as PYRPM
+
+import pulp.bindings.upload
+
+from juicer.common import Constants
+import juicer.pulp
+from juicer.pulp.Pulp import Pulp
 
 
 class Upload(Pulp):
     def __init__(self, connection):
         super(Upload, self).__init__(connection)
+        self.environment = None
+        self.name = None
+        self.pbar = None
 
     def upload(self, path, repo, environment):
-        pulp = UploadAPI(self.connection)
-        unit_key, unit_metadata = self.generate_upload_data(path)
-        name = os.path.basename(path)
+        self.name = os.path.basename(path)
+        self.environment = environment
         repo_id = "{0}-{1}".format(repo, environment)
-
-        ################################################################
-        # Initialize upload
-        ################################################################
-        response = pulp.initialize_upload()
-        if response.response_code == Constants.PULP_POST_CREATED:
-            self.output.debug("Initialized upload process for %s" % name)
-        else:
-            raise SystemError("Failed to initialize upload process for %s" %
-                              name)
-        upload_id = response.response_body['upload_id']
-
-        ################################################################
-        # Upload chunks w/ Constants.UPLOAD_AT_ONCE size
-        ################################################################
         size = os.path.getsize(path)
+        unit_key, unit_metadata = self.generate_upload_data(path)
+
+        # An array of widgets to design our progress bar.
+        widgets = ['Juicer Upload (%s): ' % self.name,
+                   progressbar.Percentage(), ' ',
+                   progressbar.Bar(marker=progressbar.RotatingMarker()), ' ',
+                   progressbar.ETA(), ' ',
+                   bitmath.integrations.BitmathFileTransferSpeed()]
+
+        # Set up the progress bar.
+        self.pbar = progressbar.ProgressBar(
+            widgets=widgets,
+            maxval=int(size)).start()
+
+        # Initialize upload.
+        upload_id = self.initialize_upload()
+
+        # Upload chunks w/ Constants.UPLOAD_AT_ONCE size.
         rpm_fd = open(path, 'rb')
         total_seeked = 0
         rpm_fd.seek(0)
-
         while total_seeked < size:
             chunk = rpm_fd.read(Constants.UPLOAD_AT_ONCE)
             last_offset = total_seeked
             total_seeked += len(chunk)
+            self.output.debug("Seeked %s data... (total seeked: %s)" %
+                              (len(chunk), total_seeked))
+            self.upload_segment(upload_id, last_offset, chunk)
+            # Update the progress bar as we go.
+            if total_seeked < self.pbar.maxval:
+                self.pbar.update(int(total_seeked))
 
-            self.output.notice("Seeked %s data... (total seeked: %s)" %
-                               (len(chunk), total_seeked))
+        # Import upload.
+        self.import_upload(upload_id, repo_id, 'rpm', unit_key, unit_metadata)
 
-            response = pulp.upload_segment(upload_id, last_offset, chunk)
-            if response.response_code is not Constants.PULP_PUT_OK:
-                self.output.error("Failed to upload %s" % name)
-                raise SystemError("Failed to upload %s" % name)
+        # Finalize upload by cleaning up request on server.
+        self.delete_upload(upload_id)
 
-        ################################################################
-        # Import upload
-        ################################################################
-        response = pulp.import_upload(upload_id,
-                                      repo_id,
-                                      'rpm',
-                                      unit_key,
-                                      unit_metadata)
-        if response.response_code not in [Constants.PULP_POST_OK,
-                                          Constants.PULP_POST_ACCEPTED]:
-            self.output.error("Failed to import upload for %s" % name)
-            raise SystemError("Failed to import upload for %s" % name)
-
-        self.output.debug("RPM upload %s complete" % name)
-
-        ################################################################
-        # Finalize upload by cleaning up request on server
-        ################################################################
-        response = pulp.delete_upload(upload_id)
-        if response.response_code != Constants.PULP_DELETE_OK:
-            self.output.error("Failed to clean up upload for %s" % name)
-            raise SystemError("Failed to clean up upload for %s" % name)
-
-        ################################################################
-        # Publish the repo
-        ################################################################
-        pulp_repo = Repo(self.connection)
+        # Publish the repo.
+        pulp_repo = juicer.pulp.Repo.Repo(self.connection)
         pulp_repo.publish(name=repo,
                           environment=environment)
 
-        ################################################################
         # FIN
-        ################################################################
-        self.output.info("successfully uploaded %s" % name)
+        self.output.info("successfully uploaded %s" % self.name)
+
+    def initialize_upload(self):
+        _pulp = pulp.bindings.upload.UploadAPI(self.connection)
+        response = _pulp.initialize_upload()
+        if response.response_code == Constants.PULP_POST_CREATED:
+            self.output.debug("Initialized upload process for %s" % self.name)
+        else:
+            raise SystemError("Failed to initialize upload process for %s" %
+                              self.name)
+        return response.response_body['upload_id']
+
+    def upload_segment(self, upload_id, last_offset, chunk):
+        _pulp = pulp.bindings.upload.UploadAPI(self.connection)
+        response = _pulp.upload_segment(upload_id, last_offset, chunk)
+        if response.response_code is not Constants.PULP_PUT_OK:
+            self.output.error("Failed to upload %s" % self.name)
+            raise SystemError("Failed to upload %s" % self.name)
+
+    def import_upload(self, upload_id, repo_id, unit_type, unit_key, unit_metadata):
+        _pulp = pulp.bindings.upload.UploadAPI(self.connection)
+        response = _pulp.import_upload(upload_id,
+                                       repo_id,
+                                       'rpm',
+                                       unit_key,
+                                       unit_metadata)
+        if response.response_code not in [Constants.PULP_POST_OK,
+                                          Constants.PULP_POST_ACCEPTED]:
+            self.output.error("Failed to import upload for %s" % self.name)
+            raise SystemError("Failed to import upload for %s" % self.name)
+
+    def delete_upload(self, upload_id):
+        _pulp = pulp.bindings.upload.UploadAPI(self.connection)
+        response = _pulp.delete_upload(upload_id)
+        if response.response_code != Constants.PULP_DELETE_OK:
+            self.output.error("Failed to clean up upload for %s" % self.name)
+            raise SystemError("Failed to clean up upload for %s" % self.name)
 
     def generate_upload_data(self, path, checksumtype='sha256'):
         rpm = PYRPM(file(path))

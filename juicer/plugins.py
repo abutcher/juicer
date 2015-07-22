fc875905b0f95ff469393cb34a807112cd2a7244
Andrew Butcher <abutcher@redhat.com>
2015-07-21 22:49:21 -0400

    remove ConfigParser import

--- juicer/plugins.py
+++ juicer/plugins.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import ConfigParser
 import imp
 import logging
 import os
